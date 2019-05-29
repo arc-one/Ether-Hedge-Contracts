7ba424fb600585971f9fe47598884436071bdf48
Vitaliy <nenzas@gmail.com>
2019-05-28 23:31:38 -0700

    Update Settings.sol

--- contracts/Settings.sol
+++ contracts/Settings.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
+
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
-
 
 contract Settings is Ownable {
     using SafeMath for uint256;
